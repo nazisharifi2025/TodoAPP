e4ed2509b5395473fb0243d4bf43b2cf9166f51f
nazi sharifi <banonazisharifi@gmail.com>
2026-01-14 21:30:42 +0430

    added new chenges

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,16 @@
 // lib/prisma.ts
 
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-const adapter = new prismaPg({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 export const prisma =
   globalForPrisma.prisma || new PrismaClient({
-    adapter:
+    adapter:adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
