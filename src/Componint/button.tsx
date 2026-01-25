1b68eebfec20545e369fa59d819c2ef64b2916f6
nazi sharifi <banonazisharifi@gmail.com>
2026-01-25 17:17:47 +0430

    added new style

---
+++ src/Componint/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function button() {
+  return (
+    <button className='px-6 py-2 border text-xl font-bold  bg-white hover:border-2'>SAve</button>
+  )
+}
+
+export default button
